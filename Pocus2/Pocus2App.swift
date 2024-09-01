81b50559ac576aa457df856ecc6ee1b3d8d5d509
Wojciech Kozioł <wojtek.koziol.pl@gmail.com>
2024-09-01 17:41:46 +0200

    Initial commit

---
+++ Pocus2/Pocus2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pocus2App.swift
+//  Pocus2
+//
+//  Created by Wojciech Kozioł on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pocus2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
